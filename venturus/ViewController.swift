d8af312577a09b5804ced5b4c4a0d496c02c0b3d
Antonio Marcos de Oliveira Junior <ajunior@apple.com>
2020-10-08 22:50:56 -0300

    Initial Commit

---
+++ venturus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  venturus
+//
+//  Created by antonio marcos on 08/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
